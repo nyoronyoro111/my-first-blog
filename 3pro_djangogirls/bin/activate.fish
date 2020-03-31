fa4715da19c88ff0aed52fc4f126ef818274e578
yuma.maron_1026@icloud.com <yuma.maron_1026@icloud.com>
2020-04-01 03:01:09 +0900

    環境変更に伴う変更

--- 3pro_djangogirls/bin/activate.fish
+++ 3pro_djangogirls/bin/activate.fish
@@ -52,7 +52,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(3pro_djangogirls) "            
+        if test -n "(3pro_djangogirls) "
             printf "%s%s" "(3pro_djangogirls) " (set_color normal)
         else
             # ...Otherwise, prepend env
